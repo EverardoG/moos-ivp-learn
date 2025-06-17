9892a8cd05f6f20d60b02fcca7acf40d487caaca
EverardoG <everardo.a.gonzalez@gmail.com>
2025-06-17 16:07:23 -0400

    checkpoint; general_utils is now a library

--- src/general_utils/general_utils.h
+++ src/general_utils/general_utils.h
@@ -1,40 +1,8 @@
-#include <iterator>
 #include "MBUtils.h"
-#include "ACTable.h"
-#include "MOOS/libMOOS/Thirdparty/AppCasting/AppCastingMOOSApp.h"
-#include "XYFormatUtilsPoint.h"
-#include "XYPoint.h"
-#include "AngleUtils.h"      // for relAngle
 #include <cmath>
 
 //-------------------------------------------------------------
 // Procedure: calcDeltaHeading(double heading1, double heading2)
 //            the delta in degrees from heading1 to heading2
 //            returns the delta heading in degrees [-180, 180]
-double calcDeltaHeading(double heading1, double heading2)
-{
-  double deg_to_rad = M_PI/180.0;
-  double rad_to_deg = 180.0/M_PI;
-
-  // Unit vectors
-  double a1 = sin(deg_to_rad*heading1);
-  double a2 = cos(deg_to_rad*heading1);
-
-  double b1 = sin(deg_to_rad*heading2);
-  double b2 = cos(deg_to_rad*heading2);
-
-  double k = a1*b2 - a2*b1;           // a cross b
-  double dot_a_b = a1*b1+ a2*b2;      // a dot b
-
-  // Clip for safety
-  dot_a_b = vclip( dot_a_b, -1.0, 1.0);
-
-  double delta_theta;
-  if (k >=0){
-    delta_theta = rad_to_deg * acos(dot_a_b) * (-1.0);
-  } else {
-    delta_theta = rad_to_deg * acos(dot_a_b) * (1.0);
-  }
-
-  return(delta_theta);
-}
+double calcDeltaHeading(double heading1, double heading2);
--- src/general_utils/test_utils.cpp
+++ src/general_utils/test_utils.cpp
@@ -2,4 +2,7 @@
 #include <iostream>
 #include <vector>
 
-int main() {}
+int main() {
+    double delta_heading = calcDeltaHeading(270.0, 90.0);
+    std::cout << "Computed delta heading: " << delta_heading << std::endl;
+}
