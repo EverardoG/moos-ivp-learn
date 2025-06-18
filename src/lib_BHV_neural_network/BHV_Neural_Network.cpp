d427e019c582c7252bc73fb8a202916c50cc3a5b
EverardoG <everardo.a.gonzalez@gmail.com>
2025-06-18 16:34:02 -0400

    remove whitespace

--- src/lib_BHV_neural_network/BHV_Neural_Network.cpp
+++ src/lib_BHV_neural_network/BHV_Neural_Network.cpp
@@ -30,7 +30,7 @@ BHV_Neural_Network::BHV_Neural_Network(IvPDomain domain) :
   addInfoVars("SECTOR_SENSOR_READING");
 
   m_best_heading = 0.0;
-  m_best_speed   = 0.0; 
+  m_best_speed   = 0.0;
 }
 
 //---------------------------------------------------------------
@@ -126,11 +126,11 @@ IvPFunction* BHV_Neural_Network::onRunState()
   // Part 1: Get the latest sensor reading from SectorSense
   bool ok_sensor_reading = processSensorReadings();
   if (!ok_sensor_reading)
-    return(0); 
-  
-  // Part 2: 
+    return(0);
+
+  // Part 2:
   forwardPropNetwork();
-  
+
   // Part 3: Build the IvP function
   IvPFunction *ipf = buildFunction();
 
@@ -142,7 +142,7 @@ IvPFunction* BHV_Neural_Network::onRunState()
 //---------------------------------------------------------------
 // Procedure: processSensorReadings()
 //   Purpose: Get the sensor reading message from the buffer,
-//            convert it, and load it into the member variable 
+//            convert it, and load it into the member variable
 //            for sensor data, m_sector_sensor_readings
 
 bool BHV_Neural_Network::processSensorReadings()
@@ -155,25 +155,25 @@ bool BHV_Neural_Network::processSensorReadings()
     postWMessage("No ownship sensor info in info_buffer.");
     return(false);
   }
-  
+
   // Parse the sensor reading into input for the neural network
   // and update the member variable
   double temp_dbl;
   bool good_reading;
- 
+
   std::vector<std::string> readings_str = parseString(sensor_input_str, ',');
 
   m_sector_sensor_readings.clear();
   for (int i=0; i<readings_str.size(); i++){
-  
+
     good_reading = setDoubleOnString(temp_dbl, readings_str[i]);
-    
+
     if(!good_reading) {
       postWMessage("Bad reading in sensor: i=" + intToString(i)
 		   + ", val=" + readings_str[i]);
       return(false);
     }
-    
+
     m_sector_sensor_readings.push_back(temp_dbl);
   }
 
@@ -186,13 +186,13 @@ bool BHV_Neural_Network::processSensorReadings()
 
 void BHV_Neural_Network::forwardPropNetwork()
 {
-  std::vector<double> outputs = m_network.forward(m_sector_sensor_readings); 
+  std::vector<double> outputs = m_network.forward(m_sector_sensor_readings);
 
   // Map from outputs to heading and speed
   // m_best_speed   = outputs[0] ?
   // m_best_heading = outputs[1] ?
-  
-  return; 
+
+  return;
 }
 
 
@@ -208,8 +208,8 @@ IvPFunction* BHV_Neural_Network::buildFunction()
   crs_zaic.setSummit(m_best_heading);
   crs_zaic.setPeakWidth(10);
   crs_zaic.setBaseWidth(10);
-  crs_zaic.setMinMaxUtil(20, 100); 
-  crs_zaic.setSummitDelta(60);   
+  crs_zaic.setMinMaxUtil(20, 100);
+  crs_zaic.setSummitDelta(60);
   crs_zaic.setValueWrap(true);
   if(crs_zaic.stateOK() == false) {
     string warnings = "Course ZAIC problems " + crs_zaic.getWarnings();
@@ -222,20 +222,20 @@ IvPFunction* BHV_Neural_Network::buildFunction()
   spd_zaic.setSummit(m_best_speed);
   spd_zaic.setPeakWidth(0.1);
   spd_zaic.setBaseWidth(0.1);
-  spd_zaic.setMinMaxUtil(20, 100); 
-  spd_zaic.setSummitDelta(60);  
+  spd_zaic.setMinMaxUtil(20, 100);
+  spd_zaic.setSummitDelta(60);
   if(spd_zaic.stateOK() == false) {
     string warnings = "Speed ZAIC problems " + spd_zaic.getWarnings();
     postWMessage(warnings);
     return(0);
   }
-  
+
   IvPFunction *spd_ipf = spd_zaic.extractIvPFunction();
   IvPFunction *crs_ipf = crs_zaic.extractIvPFunction();
 
   OF_Coupler coupler;
   IvPFunction *ipf = coupler.couple(crs_ipf, spd_ipf, 50, 50);
-  
+
   // Part N: Prior to returning the IvP function, apply the priority wt
   // Actual weight applied may be some value different than the configured
   // m_priority_wt, depending on the behavior author's insite.
