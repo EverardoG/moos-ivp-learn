e636b0fdce0ccac36d8d26d61e8cb58561055748
EverardoG <everardo.a.gonzalez@gmail.com>
2025-06-20 13:37:13 -0400

    add IvPBehaviorExtend class

---
+++ src/ivp_behavior_extend/ivp_behavior_extend.cpp
@@ -0,0 +1,30 @@
+/************************************************************/
+/*    NAME: Everardo Gonzalez                               */
+/*    ORGN: MIT                                             */
+/*    FILE: ivp_behavior_extend.cpp                          */
+/*    DATE:                                                 */
+/************************************************************/
+
+#include "ivp_behavior_extend.h"
+
+bool IvPBehaviorExtend::setVecDoubleOnString(std::vector<double> given_vec_double, const std::string& str) {
+  double temp_dbl;
+  bool good_reading;
+
+  // Turn our string into a vector of strings, using the comma as a delimiter
+  std::vector<std::string> vec_str = parseString(str, ',');
+
+  for (int i=0; i<vec_str.size(); i++){
+    // Turn string into a double
+    good_reading = setDoubleOnString(temp_dbl, vec_str[i]);
+
+    if(!good_reading) {
+      postWMessage("Bad reading in index: i=" + intToString(i)
+		   + ", val=" + vec_str[i]);
+      return(false);
+    }
+
+    given_vec_double.push_back(temp_dbl);
+  }
+  return true;
+}
---
+++ src/ivp_behavior_extend/ivp_behavior_extend.h
@@ -0,0 +1,31 @@
+/************************************************************/
+/*    NAME: Everardo Gonzalez                               */
+/*    ORGN: MIT                                             */
+/*    FILE: ivp_behavior_extend.h                          */
+/*    DATE:                                                 */
+/************************************************************/
+
+#ifndef IvP_Behavior_Extend_HEADER
+#define IvP_Behavior_Extend_HEADER
+
+#include <string>
+#include "IvPBehavior.h"
+#include "network.h"
+#include "ZAIC_PEAK.h"
+#include "OF_Coupler.h"
+#include "AngleUtils.h"
+#include "GeomUtils.h"
+
+
+// Class that extends IvPBehavior to include additional helper functions
+// as class methods
+class IvPBehaviorExtend : public IvPBehavior {
+public:
+    IvPBehaviorExtend(IvPDomain domain) : IvPBehavior(domain) {}
+    virtual ~IvPBehaviorExtend() {}
+
+    // New method
+    bool setVecDoubleOnString(std::vector<double> given_vec_double, const std::string& str);
+};
+
+#endif
